1a0c631dc08227235be95717b55f81ffad07776a
Krishan <krishandadhich11@gmail.com>
2025-12-10 11:37:25 +0530

    set up MongoDB connection with Mongoose

---
+++ src/config/db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  const uri = process.env.MONGO_URI;
+  if (!uri) throw new Error("MONGO_URI missing");
+  await mongoose.connect(uri, {
+    // recommended options handled by mongoose 6+
+  });
+  console.log("MongoDB connected");
+};
